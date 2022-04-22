42ede743da9bf3f3451e7530dff3f2f2a503fc72
our0107 <zhuo0107@foxmail.com>
2022-04-22 12:31:03 +0800

    1

---
+++ 93.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class CollectionTest {
+    public CollectionTest() {
+
+    }
+}
+
+public class test {
+    private class Student {
+        String num;
+        String name;
+        int age;
+
+        public Student() {
+        }
+
+        public Student(String num, String name, int age) {
+            this.num = num;
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    public static void main(String[] args) {
+        new CollectionTest();
+    }
+}
