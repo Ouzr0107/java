978dc054d45d057845b6c08c9586cd73e3e61ab8
Ouzr0107 <87957461+Ouzr0107@users.noreply.github.com>
2022-05-06 08:47:49 +0800

    Create 92.java

---
+++ 92.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class CollectionTest {
+    public CollectionTest() {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("James");
+        list.add("Tom");
+        list.add("Steven");
+        list.add("Alice");
+        for (int i = 0; i < list.size(); ++i) {
+            System.out.println(list.get(i) + " ");
+        }
+        Iterator it = list.iterator();
+        while (it.hasNext()) {
+            String e = (String) it.next();
+            System.out.println(e + " ");
+            if (it.next() == "Tom") {
+                it.remove();
+            }
+        }
+        Collections.sort(list);
+        for (String i : list) {
+            System.out.println(i + " ");
+        }
+    }
+}
+
+public class test {
+    public static void main(String[] args) {
+        new CollectionTest();
+    }
+}
