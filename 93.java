269df5d3ebff4fdde9e2f427fb69fae3222a02f5
Ouzr0107 <87957461+Ouzr0107@users.noreply.github.com>
2022-05-06 08:48:36 +0800

    Update 93.java

--- 93.java
+++ 93.java
@@ -1,12 +1,6 @@
 import java.util.*;
 
 class CollectionTest {
-    public CollectionTest() {
-
-    }
-}
-
-public class test {
     private class Student {
         String num;
         String name;
@@ -20,8 +14,42 @@ public class test {
             this.name = name;
             this.age = age;
         }
+
+        @Override
+        public String toString() {
+            return this.num + " " + this.name + " " + this.age;
+        }
     }
 
+    public CollectionTest() {
+        Map<String, Student> stu = new HashMap<>();
+        stu.put("15H002", new Student("15H002", "孙悟空", 2000));
+        stu.put("15H001", new Student("15H001", "唐僧", 40));
+        stu.put("15H003", new Student("15H003", "猪八戒", 1000));
+        if (stu.containsKey("15H003")) {
+            System.out.println(stu.get("15H003").num + " " + stu.get("15H003").name + " " + stu.get("15H003").age);
+        } else {
+            System.out.println("15H003不存在");
+        }
+        if (stu.containsKey("15H004")) {
+            System.out.println(stu.get("15H004").num + " " + stu.get("15H004").name + " " + stu.get("15H004").age);
+        } else {
+            System.out.println("15H004不存在");
+        }
+        for (Map.Entry<String, Student> entry : stu.entrySet()) {
+            String key = entry.getKey();
+            Student value = entry.getValue();
+            System.out.println(key + " " + value.name + " " + value.age);
+        }
+        stu.remove("15H001");
+        Object[] values = stu.values().toArray();
+        for (int i = 0; i < values.length; ++i) {
+            System.out.println(values[i].toString());
+        }
+    }
+}
+
+public class test {
     public static void main(String[] args) {
         new CollectionTest();
     }
