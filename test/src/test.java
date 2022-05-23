3ee773486ab9e2e2e75f1340f2c2259a04c04705
Ouzr0107 <zhuo0107@foxmail.com>
2022-05-23 09:58:05 +0800

    123

--- test/src/test.java
+++ test/src/test.java
@@ -1,14 +1,11 @@
 import java.awt.*;
 import javax.swing.*;
-import java.lang.*;
 
 public class test {
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(() -> {
 			var frame = new Frame();
-			frame.setTitle("我的第一个窗口");
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setVisible(true);
 		});
 	}
@@ -17,6 +14,8 @@ public class test {
 
 class Frame extends JFrame {
 	public Frame() {
+		setTitle("计算器");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Toolkit kit = Toolkit.getDefaultToolkit();
 		Dimension screenSize = kit.getScreenSize();
 		int screenWidth = screenSize.width;
@@ -24,26 +23,16 @@ class Frame extends JFrame {
 		setSize(screenWidth * 2 / 3, screenHeight * 2 / 3);
 		Image img = new ImageIcon("src\\ico.png").getImage();
 		setIconImage(img);
-		add(new testComponent());
-	}
-}
-
-class testComponent extends JComponent {
-	public void paintComponent(Graphics g) {
-		var message = "This is my first graphic:";
-		var f = new Font("Arial", Font.BOLD + Font.ITALIC, 16);
-		g.setFont(f);
-		g.setColor(Color.RED);
-		g.drawString(message, 100, 20);
-		g.setColor(Color.BLUE);
-		g.fillRect(50, 50, 100, 50);
-		g.setColor(Color.RED);
-		g.drawLine(160, 75, 300, 75);
-		g.drawLine(250, 50, 300, 75);
-		g.drawLine(250, 100, 300, 75);
-		g.setColor(Color.GREEN);
-		g.drawArc(310, 50, 50, 50, 0, 360);
-		g.setColor(Color.BLACK);
-		g.drawArc(20, 25, 350, 100, 0, 360);
+		JPanel jp = new JPanel();
+		var lo = new 
+		jp.setLayout();
+		add(jp);
+		var tf1 = new JTextField(5);
+		var opt = new String[] { "+", "-", "*", "/" };
+		var cb1 = new JComboBox<String>(opt);
+		var tf2 = new JTextField(5);
+		var lb1 = new JLabel("=");
+		var tf3 = new JTextField(10);
+		
 	}
 }
